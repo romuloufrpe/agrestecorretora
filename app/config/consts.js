477a3e8c6911b8bbc1d6de4d8f26170e43cd66df
romuloufrpe <romulo.ferreira.ufrpe@gmail.com>
2018-08-14 12:39:25 -0300

    config restaurado

--- app/config/consts.js
+++ app/config/consts.js
@@ -2,13 +2,10 @@ angular.module('primeiraApp').constant('consts', {
   appName: 'Agreste Corretora',
   version: '1.0',
   owner: 'Urmininu',
+  apiURL:'https://agreste-b.herokuapp.com/api',
+  oapiURL: 'https://agreste-b.herokuapp.com/oapi'
   year: '2017',
   userKey: '_primeira_app_user'
 }).run(['$rootScope', 'consts', function($rootScope, consts) {
   $rootScope.consts = consts
 }])
-
-export default {
-    API_URL: 'https://agreste-b.herokuapp.com/api',
-    OAPI_URL: 'https://agreste-b.herokuapp.com/oapi',
-}
