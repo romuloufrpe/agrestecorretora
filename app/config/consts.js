b2d514aaf9dac387f5639d8d81f14bbde4e4baa3
romuloufrpe <romulo.ferreira.ufrpe@gmail.com>
2018-08-14 10:32:20 -0300

    removido oapi

--- app/config/consts.js
+++ app/config/consts.js
@@ -4,7 +4,6 @@ angular.module('primeiraApp').constant('consts', {
   owner: 'Urmininu',
   year: '2017',
   apiUrl: 'https://agreste-b.herokuapp.com/api',
-  oapiUrl: 'https://agreste-b.herokuapp.com/oapi',
   userKey: '_primeira_app_user'
 }).run(['$rootScope', 'consts', function($rootScope, consts) {
   $rootScope.consts = consts
